86aff85cef4dcc2bbe9b342f7093ac0b680881e1
songzhenyi <songzhenyi@aliyun.com>
2018-07-24 05:55:56 +0800

    add before notice

--- jCode/MavenCheck/src/main/java/com/honliv/hp/it/aop/Main.java
+++ jCode/MavenCheck/src/main/java/com/honliv/hp/it/aop/Main.java
@@ -13,14 +13,14 @@ public class Main
 		// TODO Auto-generated method stub
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 		ArithmeticCalculator arithmeticCalculator = (ArithmeticCalculator) ctx.getBean("arithmeticCalculator");
-		((ConfigurableApplicationContext)ctx).close();
+		((ConfigurableApplicationContext) ctx).close();
 		System.out.println(arithmeticCalculator.getClass().getName());
-		
+
 		int result = arithmeticCalculator.add(1, 2);
 		System.out.println("result:" + result);
-		
-		result = arithmeticCalculator.div(1000, 10);
-		System.out.println("result:" + result);
+
+//		result = arithmeticCalculator.div(1000, 10);
+//		System.out.println("result:" + result);
 	}
 
 }
