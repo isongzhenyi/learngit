9f95159638c46f01498a366387d26d781bdcb01b
songzhenyi <songzhenyi@aliyun.com>
2018-07-26 06:45:39 +0800

    add log4j and documents files

--- jCode/MavenCheck/src/main/java/com/honliv/hp/MavenCheck/App.java
+++ jCode/MavenCheck/src/main/java/com/honliv/hp/MavenCheck/App.java
@@ -1,5 +1,6 @@
 package com.honliv.hp.MavenCheck;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
  
@@ -11,6 +12,8 @@ public class App
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Car car = (Car) context.getBean("car");
 		System.out.println(car);
-          
+        Logger logger = Logger.getLogger(Car.class)   ;
+        logger.info("this is my info");
+        logger.info(car);
 	}
 }
