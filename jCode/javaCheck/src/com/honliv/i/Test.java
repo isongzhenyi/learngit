c0d4de2ca568a730c94644016012b03cc4ed70ed
songzhenyi <songzhenyi@aliyun.com>
2018-07-25 09:55:20 +0800

    完善了单元测试

--- jCode/javaCheck/src/com/honliv/i/Test.java
+++ jCode/javaCheck/src/com/honliv/i/Test.java
@@ -1,15 +1,34 @@
 package com.honliv.i;
-
+ 
 public class Test
 {
 	@org.junit.Test
 	public void name()
 	{
-//		System.out.println("this is my first test");
 		TestClass testClass = new TestClass();
 		testClass.setMyAge((short) 30);
 		testClass.setMyName("songzhneyi");
 		System.out.println(testClass);
 	}
 
+	@org.junit.Test
+	public void name2()
+	{
+		TestClass testClass = new TestClass();
+		testClass.setMyAge((short) 30);
+		testClass.setMyName("hanliwen");
+		System.out.println(testClass);
+	}
+	
+	@org.junit.Before
+	public void Before()
+	{
+		System.out.println("Before");
+	}
+	
+	@org.junit.After
+	public void After()
+	{		 
+		System.out.println("After");
+	}
 }
