55f58bb030f981c544febef9ba23762fd0ff806f
songzhenyi <songzhenyi@aliyun.com>
2018-08-06 21:32:21 +0800

    ForumService

---
+++ jCode/MavenCheck/src/main/java/com/honliv/hp/it/aop/ForumService.java
@@ -0,0 +1,16 @@
+package com.honliv.hp.it.aop;
+
+public class ForumService
+{
+	@NeedTest(value=true)
+	public void deleteForum(int forumId)
+	{
+		System.out.println("删除论坛模块："+forumId);
+	}
+
+	@NeedTest(value=false)
+	public void deletTopic(int postId)
+	{
+		System.out.println("删除论坛主题："+postId);
+	}
+}
