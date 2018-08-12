3a30bffa06abd3ea059c4b56b227de4a6387e345
songzhenyi <songzhenyi@aliyun.com>
2018-08-13 07:06:24 +0800

    test atouwired

---
+++ jCode/MavenCheck/src/main/java/com/honliv/hp/it/aop/annotation/Autowired/TextEditor.java
@@ -0,0 +1,24 @@
+package com.honliv.hp.it.aop.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor
+{
+	private SpellChecker spellChecker;
+
+	@Autowired
+	public void setSpellChecker(SpellChecker spellChecker)
+	{
+		this.spellChecker = spellChecker;
+	}
+
+	public SpellChecker getSpellChecker()
+	{
+		return spellChecker;
+	}
+
+	public void spellCheck()
+	{
+		spellChecker.checkSpelling();
+	}
+}
