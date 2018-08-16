f2a90aefb3c07cec49d57e041d761ce326ede495
songzhenyi <songzhenyi@aliyun.com>
2018-08-17 05:49:07 +0800

    delete some  files

--- cCode/Sikiro.Tookits.Files-master/Sikiro.Tookits.Files.Sample.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sikiro.Tookits.Files.Sample.Web.MvcApplication" Language="C#" %>
--- cCode/Sikiro.Tookits.Files-master/Sikiro.Tookits.Files/ImportEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sikiro.Tookits.Files
-{
-    internal class ImportEntity
-    {
-        public string PropertyName { get; set; }
-
-        public string DisplayName { get; set; }
-    }
-}
